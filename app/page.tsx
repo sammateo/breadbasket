df000f510efe719f60188dab00ba40f01197b791
Mateo Sam <sammateo4@gmail.com>
2025-06-08 14:21:35 -0400

    lint fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import { Landing } from "@/components/Landing/Landing";
 import { FeaturedLists } from "@/components/Lists/FeaturedLists";
 import { NavigationBar } from "@/components/Navigation/NavigationBar";
-import Image from "next/image";
 
 export default function Home() {
 	return (
